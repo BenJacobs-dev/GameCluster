ec99518dd70d59dea7098e386f9baeb89ba7b8aa
bjacobsUNB <bjacobs@UNB.ca>
2021-10-16 17:30:26 -0300

    Commit FractalMaker
    Fractal Maker currently only displays the mandolbrot set in full

--- src/patternMaker/FractalMaker.java
+++ src/patternMaker/FractalMaker.java
@@ -20,21 +20,23 @@ import javafx.scene.paint.*;
 import javafx.scene.image.*;
 import javafx.stage.*;
 
-public class PatternMaker extends Application{
+public class FractalMaker extends Application{
 
 	///////////////////////////////////////////////
 	//                Edit here                  //
 	///////////////////////////////////////////////
 	
-	int modNum = 17, screen = 500, pixelSize = 8, blackStart = 10, fps = 1000;
-	double xMulti = 1.75, multiIn = 0.00001;
+	int modNum = 8, screen = 1000, pixelSize = 1, blackStart = 10, fps = 1000;
+	double rightShift = .5, downShift = 0;
+	double xMulti = 1.5, multiIn = 0.00001, displayBounds = 1.25;
+	boolean autoIterate = false; //true;
 	
 	///////////////////////////////////////////////
 	
 	int size = screen/pixelSize, sizeX = (int)(size*xMulti), sizeY = size, mapSizeX = (int)(xMulti*screen/sizeX), mapSizeY = screen/sizeY, displaySizeX = sizeX*mapSizeX,  displaySizeY = sizeY*mapSizeY, fpsMulti = 1000/fps;
 
 	Stage stage;
-	int grid[][] , gridIndex[];
+	double grid[][][];
 	Rectangle player;
 	HashMap<KeyCode, Runnable> keyActions;
 	LinkedList<KeyCode> keysPressed;
@@ -64,20 +66,29 @@ public class PatternMaker extends Application{
 
 		Rectangle mapRect = new Rectangle(0, 0, displaySizeX, displaySizeY);
 		
-		/*
-		grid = new int[sizeX][sizeY];
-		for(int i = 0, j; i < sizeX; i++) {
+		grid = new double[sizeX][sizeY][6];
+		for(int i = 0, j, hSizeX = sizeX>>1, hSizeY = sizeY>>1; i < sizeX; i++) {
 			for(j = 0; j < sizeY; j++) {
-				grid[i][j] = (int)((i*j*multi*multi)/10%modNum);
+				grid[i][j][1] = (displayBounds*(i-hSizeX))/hSizeY-rightShift;
+				grid[i][j][2] = (displayBounds*(j-hSizeY))/hSizeY+downShift;
+				grid[i][j][5] = Math.sqrt(grid[i][j][1]*grid[i][j][1] + grid[i][j][2]*grid[i][j][2]);
 			}
 		}
-		*/
 		
 		WritableImage img = new WritableImage(displaySizeX, displaySizeY);
 		imgWriter = img.getPixelWriter();
 		mapRect.setFill(new ImagePattern(img));
-		//updateGrid();
+
+		Button iterButton = new Button("Iterate");
+		iterButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				updateGrid();
+				updateDisplay();
+			}
+		});
 		
+		/*
 		TextField valTextIn = new TextField();
 		valTextIn.setAlignment(Pos.CENTER);
 		valTextIn.setOnAction(new EventHandler<ActionEvent>() {
@@ -121,6 +132,7 @@ public class PatternMaker extends Application{
 						multi += valSliderIn.getValue();
 					}
 					
+					
 					//valTextIn.setText(valTextIn.getText()+((int)(valSliderIn.getValue())));
 					//updateGrid();
 				}
@@ -129,61 +141,94 @@ public class PatternMaker extends Application{
 		
 		VBox inputBox = new VBox(valTextIn, valSliderIn);
 		inputBox.setAlignment(Pos.BOTTOM_LEFT);
+		*/
 		
 		nodeList.add(mapRect);
-		nodeList.add(inputBox);
+		nodeList.add(iterButton);
 
 		Scene scene = new Scene(nodeGroup, displaySizeX, displaySizeY);
 
 		stage.setScene(scene);
 		stage.show();
+		updateDisplay();
 		
-		Task<Void> task = new Task<Void>() {
-
-			long time;
-
-			@Override
-			protected Void call() throws Exception {
-				try {
-					time = System.currentTimeMillis();
-					running.set(true);
-					while(running.get()) {
-						semaphore.acquire();
-						Thread.sleep(Math.max(fpsMulti+time-System.currentTimeMillis(), 0));
+		if(autoIterate) {
+			Task<Void> task = new Task<Void>() {
+	
+				long time;
+	
+				@Override
+				protected Void call() throws Exception {
+					try {
 						time = System.currentTimeMillis();
-						
-						multi += multiIn;
-						
-						Platform.runLater(() -> {
-							//updateGrid();
-							updateDisplay();
-							semaphore.release();
-						});
-					}
-				}catch(Exception e){e.printStackTrace();}
-				return null;
-			}
-		};
-
-		new Thread(task).start();
-
+						running.set(true);
+						while(running.get()) {
+							semaphore.acquire();
+							Thread.sleep(Math.max(fpsMulti+time-System.currentTimeMillis(), 0));
+							time = System.currentTimeMillis();
+							
+							multi += multiIn;
+							
+							Platform.runLater(() -> {
+								updateGrid();
+								updateDisplay();
+								semaphore.release();
+							});
+						}
+					}catch(Exception e){e.printStackTrace();}
+					return null;
+				}
+			};
+	
+			new Thread(task).start();
+		}
 	}
 
-	/*
-	public void updateGrid() {
+	public void updateGridOld() {
+		double xTemp;
 		for(int i = 0, j; i < sizeX; i++) {
 			for(j = 0; j < sizeY; j++) {
-				grid[i][j] =  grid[(i+1)%sizeX][(j+1)%sizeY];
+				for(int count = 100; count >= 0; count--)
+				if(grid[i][j][5] != Double.POSITIVE_INFINITY ) {
+					grid[i][j][0]++;
+					xTemp = grid[i][j][3]*grid[i][j][3]-grid[i][j][4]*grid[i][j][4]+grid[i][j][1];
+					grid[i][j][4] = 2*grid[i][j][3]*grid[i][j][4]+grid[i][j][2];
+					grid[i][j][3] = xTemp;
+					grid[i][j][5] = Math.sqrt(grid[i][j][3]*grid[i][j][3] + grid[i][j][4]*grid[i][j][4]);
+				}else {
+					break;
+				}
 			}
 		}
 	}
-	*/
+	
+	public void updateGrid() {
+		double x2, y2;
+		for(int i = 0, j; i < sizeX; i++) {
+			for(j = 0; j < sizeY; j++) {
+				x2 = grid[i][j][3]*grid[i][j][3];
+				y2 = grid[i][j][4]*grid[i][j][4];
+				for(int count = 100; count >= 0; count--)
+				if(x2+y2 <= 4) {
+					grid[i][j][0]++;
+					grid[i][j][4] = (grid[i][j][3]+grid[i][j][3])*grid[i][j][4]+grid[i][j][2];
+					grid[i][j][3] = x2-y2+grid[i][j][1];
+					x2 = grid[i][j][3]*grid[i][j][3];
+					y2 = grid[i][j][4]*grid[i][j][4];
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
 	
 	public void updateDisplay() {
 		for(int i = 0, j; i < sizeX; i++) {
 			for(j = 0; j < sizeY; j++) {
-				setSquareColor((int)i, (int)j, colorList.get((int)((i*j*multi*multi)/10%modNum)));
-				//setSquareColor(i, j, colorList.get(grid[i][j]));
+				//setSquareColor((int)i, (int)j, colorList.get((int)((i*j*multi*multi)/10%modNum)));
+				//setSquareColor(i, j, colorList.get((int)grid[i][j][0]%modNum));
+				setSquareColor(i, j, colorList.get((int)(grid[i][j][0]%modNum)));
 			}
 		}
 	}
